ef0a21d2be0ecf99b2238769aef90935e2b6f508
Hari Krishnan VR <hari.krishnan@infinitylearn.com>
2023-08-15 12:55:45 +0530

    feat : boardcast event from server and consuming from client

--- api/index.js
+++ api/index.js
@@ -3,7 +3,11 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:8080"
+    }
+});
 
 app.get('/', (req, res) => {
     res.send("Hello")
@@ -13,6 +17,16 @@ io.on('connection', (socket) => {
     console.log('a user connected');
 });
 
+const broadcastTime = ()=>{
+    setInterval(() => {
+        const data = new Date().toISOString();
+        console.debug("⌚ emitting time event", data)
+        io.emit("time", data);
+    }, 1000);
+}
+
 server.listen(3000, () => {
     console.log('🚀 server listening on *:3000');
 });
+
+broadcastTime();
--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { socket } from './socket'
+
+
+ 
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [isConnected, setIsConnected] = useState(socket.connected);
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onTimeChangeEvent(value) {
+      setTime(value);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('time', onTimeChangeEvent);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('foo', onTimeChangeEvent);
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>React websocket poc</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <p>
+          {isConnected? "Connected": "Not connected"}
         </p>
+        <p>Time : {time}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
